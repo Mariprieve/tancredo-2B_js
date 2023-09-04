65f95f4887b99398d14061d9675346d7e1df9570
Mari Prieve <127523838+Mariprieve@users.noreply.github.com>
2023-09-04 10:47:56 -0300

    aula 04/09

---
+++ main.js
@@ -0,0 +1,19 @@
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play()
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play()
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play()
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
