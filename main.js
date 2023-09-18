9e759f79ba8572fd14c4ec7b803755efc30e1606
Mari Prieve <127523838+Mariprieve@users.noreply.github.com>
2023-09-18 10:40:44 -0300

    18/09

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play()
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
 
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play()
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`; 
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    console.log(contador);
+
+    onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add ('ativa');
+        }
+        
+    }
+
+    onkeyup = function () {
+        tecla.classList.remove ('ativa');
+    }
 }
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play()
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
